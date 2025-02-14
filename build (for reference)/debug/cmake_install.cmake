56bab280f08cd761d4f1f824e19372eb5545ab9c
Frank Bauernoeppel <frank@bauernoeppel.de>
2025-02-14 08:07:29 +0100

    build (for reference) added

--- build (for reference)/debug/cmake_install.cmake
+++ build (for reference)/debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/frank/OneDrive/Lehre/RO MPT ES/RO/blinky_bare_metal_c
+# Install script for directory: C:/Users/frank/OneDrive/Lehre/RO MPT ES/blinky_bare_metal_c
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/ST/STM32CubeCLT_1.15.1/GNU-tools-for-STM32/bin/arm-none-eabi-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/ST/STM32CubeCLT_1.17.0/GNU-tools-for-STM32/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/frank/OneDrive/Lehre/RO MPT ES/RO/blinky_bare_metal_c/build/debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/frank/OneDrive/Lehre/RO MPT ES/blinky_bare_metal_c/build/debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
